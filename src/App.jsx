bc73adf196a354b74ac2d0689d40d4baa127a986
muhammadyahyo <74825893+muhammadyahyo@users.noreply.github.com>
2024-04-24 11:35:11 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar';
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
+import UserManagementPage from './pages/UserManagment';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signin" element={<LoginPage />} />
+          <Route path="/signup" element={<RegistrationPage />} />
+          <Route path="/user-management" element={<UserManagementPage />} />
+          <Route path='*' element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from "react-query";
-import App from './App.tsx';
+import App from './App.jsx';
 import './index.css';
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
